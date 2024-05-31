69b0aebc1c029d04c5e95a257260df0d0f829a23
Josh W <wilson.jd022@gmail.com>
2024-05-31 12:51:11 -0700

    Updated torque_force to torque because reasons

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ class_name Player
 extends RigidBody2D
 
 @export var force = 500
-@export var torque_force = 10000
+@export var torque = 10000
 
 func _physics_process(delta: float) -> void:
 	_handle_player_input()
@@ -13,8 +13,7 @@ func _handle_player_input() -> void:
 	if (Input.is_action_pressed("player_move_up")):
 		apply_force(Vector2(sin(rotation) * force, cos(rotation) * -force))
 	if (Input.is_action_pressed("player_move_left")):
-		apply_torque(-torque_force)
+		apply_torque(-torque)
 	if (Input.is_action_pressed("player_move_right")):
-		apply_torque(torque_force)
+		apply_torque(torque)
 	
-
