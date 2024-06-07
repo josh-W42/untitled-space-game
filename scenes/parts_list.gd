8e9498dd3e3aed45b38b61023c04473fd1134b9b
Josh W <wilson.jd022@gmail.com>
2024-06-07 08:26:04 -0700

    the textures for the parts import correctly with respect to each of their sizes

--- scenes/parts_list.gd
+++ scenes/parts_list.gd
@@ -6,29 +6,11 @@ var small_parts_path = Constants.FILE_PATH_ASSETS + "parts/"
 var large_parts_path = Constants.FILE_PATH_ASSETS + "parts_2x/"
 
 func _ready() -> void:
-	for ship_part in dir_contents():
+	for ship_part in Helpers.Get_files_from_dir(small_parts_path):
 		var texture = load(small_parts_path + ship_part)
 		var texture_rect = TextureRect.new()
 		texture_rect.texture = texture
+		texture_rect.stretch_mode = TextureRect.STRETCH_KEEP
+		texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
 		
 		grid.add_child(texture_rect)
-		
-	
-func dir_contents() -> Array[String]:
-	print(small_parts_path)
-	var dir = DirAccess.open(small_parts_path)
-	if dir == null:
-		print("An error occurred when trying to access the path.")
-		return []
-		
-	var paths: Array[String] = []
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		if dir.current_is_dir():
-			print("Found directory: " + file_name)
-		elif file_name.ends_with("png"):
-			paths.append(file_name)
-		file_name = dir.get_next()
-		
-	return paths
